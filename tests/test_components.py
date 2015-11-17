6412e75816d6b31fe83eb732235674511c64215f
bibi21000 <bibi21000@gmail.com>
2015-11-17 19:31:22 +0100

    Add tests

--- tests/test_components.py
+++ tests/test_components.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""Unittests for Janitoo-Samsung Server.
+"""Unittests for Janitoo-Roomba Server.
 """
 __license__ = """
     This file is part of Janitoo.
@@ -30,8 +30,9 @@ import threading
 import logging
 from pkg_resources import iter_entry_points
 
-from janitoo_nosetests.component import JNTTComponent, JNTTComponentCommon
 from janitoo_nosetests.server import JNTTServer, JNTTServerCommon
+from janitoo_nosetests.thread import JNTTThread, JNTTThreadCommon
+from janitoo_nosetests.component import JNTTComponent, JNTTComponentCommon
 
 from janitoo.utils import json_dumps, json_loads
 from janitoo.utils import HADD_SEP, HADD
@@ -40,7 +41,7 @@ from janitoo.utils import TOPIC_NODES, TOPIC_NODES_REPLY, TOPIC_NODES_REQUEST
 from janitoo.utils import TOPIC_BROADCAST_REPLY, TOPIC_BROADCAST_REQUEST
 from janitoo.utils import TOPIC_VALUES_USER, TOPIC_VALUES_CONFIG, TOPIC_VALUES_SYSTEM, TOPIC_VALUES_BASIC
 
-JNTTServer.onlyRasperryTest()
+import janitoo_hostsensor_raspberry.component as jnt_component
 
 ##############################################################
 #Check that we are in sync with the official command classes
@@ -52,25 +53,7 @@ COMMAND_DISCOVERY = 0x5000
 assert(COMMAND_DESC[COMMAND_DISCOVERY] == 'COMMAND_DISCOVERY')
 ##############################################################
 
-class TestHardwareCpu(JNTTComponent, JNTTComponentCommon):
-    """Test the Hardware CPU component
+class TestComponentPiCPU(JNTTComponent, JNTTComponentCommon):
+    """Test the component
     """
-    component_name = 'hostsensor.picpu'
-
-    def test_100_hardware_cpu_temperature(self):
-        #self.skipRaspiTest()
-        component = self.factory[self.component_name]()
-        print("Temperature : ", component.cpu_temperature(None, 0))
-        self.assertNotEqual(component.cpu_temperature(None, 0), None)
-
-    def test_110_hardware_cpu_frequency(self):
-        #self.skipRaspiTest()
-        component = self.factory[self.component_name]()
-        print("Frequency : ", component.cpu_frequency(None, 0))
-        self.assertNotEqual(component.cpu_frequency(None, 0), None)
-
-    def test_120_hardware_cpu_volt(self):
-        #self.skipRaspiTest()
-        component = self.factory[self.component_name]()
-        print("Voltage : ", component.cpu_volt(None, 0))
-        self.assertNotEqual(component.cpu_volt(None, 0), None)
+    component_name = "hostsensor.picpu"
